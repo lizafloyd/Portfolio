869af38e2844c83f7970fb22eb17b80001ff6d3f
Liza Floyd <lizafloyd@gmail.com>
2016-12-08 20:56:31 -0500

    completes angular project states

--- js/app.js
+++ js/app.js
@@ -11,15 +11,33 @@ angular
 ])
 
 function Router ($stateProvider) {
+  console.log('hitting router');
   $stateProvider
   .state('home', {
     url: '/',
+    templateUrl: 'js/ng-views/home.html',
     controller: 'home',
     controllerAs: 'vm'
+  })
+  .state('wanderfully', {
+    url:'/wanderfully',
+    templateUrl: "js/ng-views/wanderfully.html"
+  })
+  .state('connectplus', {
+    url:'/connectplus',
+    templateUrl: "js/ng-views/connectplus.html"
+  })
+  .state('simon', {
+    url:'/simon',
+    templateUrl: "js/ng-views/simon.html"
+  })
+  .state('sevenforks', {
+    url:'/sevenforks',
+    templateUrl: "js/ng-views/sevenforks.html"
   })
 }
 
 function home (){
   console.log("home controller");
-  
+
 }
